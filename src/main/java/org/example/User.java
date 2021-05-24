74628d6b72c31d42e5fd54d090ac00920cc9152d
Ramil Murtazin <murtazinram@gmail.com>
2021-05-24 17:48:22 +0300

    add getter and setter

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -6,4 +6,12 @@ public class User {
     public User(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
